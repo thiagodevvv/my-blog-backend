5b91b67544e5e6f25c7e6028923605018d9da962
lemao-code <th.ferrari@outlook.com.br>
2021-08-30 08:25:17 -0300

    add likes and comments in post

--- src/models/Post.js
+++ src/models/Post.js
@@ -12,6 +12,14 @@ const PostSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    likes: {
+        type: Number,
+        required: true
+    },
+    comments: {
+        type: [],
+        required: true
+    }
 }, {timestamps: true})
 
 module.exports = mongoose.model("posts", PostSchema)
--- src/routes/index.js
+++ src/routes/index.js
@@ -7,18 +7,23 @@ router.get('/', async (req,res) => {
         const data = await Post.find({})
         if(data.length > 0 ) return res.status(200).send(data)
     }catch(err) {
-       if(err) throw err
+       if(err) console.log(`Erro ao pegar todos posts: ${err}`)
     }
 })
 
 router.post('/create', async (req,res) => {
     try {
-        const data = await Post.create(req.body.post)
+        const post = {
+            title: `${req.body.title}`,
+            description: `${req.body.description}`,
+            likes: 0,
+            comments: []
+        }
+        const data = await Post.create(post)
         if(data._id) return res.status(200).send(data)
     }catch(err) {
         if(err) console.log(`Error: ${err}`)
     }
 })
-
 
 module.exports = router
