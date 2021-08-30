9e9f550f4f626db29e06cfa2a3de9b663e065328
lemao-code <th.ferrari@outlook.com.br>
2021-08-30 18:56:41 -0300

    insert pages static with express and create pages: login, posts and guest

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ require('dotenv').config()
 
 const express = require('express')
 const cors = require('cors')
+const path =  require('path')
 const app = express()
 
 const PORT = 8000
 
 app.use(cors())
 app.use(express.json())
+
+app.use(express.static(path.join(__dirname, '/src/routes/public')))
 app.use(require('./src/routes'))
 
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,23 +1,57 @@
 const router = require('express').Router()
 const Post = require('../models/Post')
+const axios = require('axios')
+const path = require('path')
+const mongoose = require('mongoose')
+
 
 
 router.get('/', async (req,res) => {
-    try {
-        const data = await Post.find({})
-        if(data.length > 0 ) return res.status(200).send(data)
-    }catch(err) {
-       if(err) console.log(`Erro ao pegar todos posts: ${err}`)
+    res.sendFile(path.join(__dirname, '/public/login.html'))
+})
+router.get('/posts/:token', (req,res) => {
+    if(req.params) {
+        res.sendFile(path.join(__dirname, '/public/posts.html'))
     }
 })
 
+router.get('/guest/posts', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/guest.html'))
+})
+router.get('/auth', async(req,res) => {
+   res.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.CLIENT_ID_GITHUB}`)  
+})
+
+router.get('/oauth-callback', async ({query: {code}},res) => {
+    const opts = { 
+        headers: { accept: 'application/json' }, 
+    }
+    const body =  {
+        client_id: process.env.CLIENT_ID_GITHUB,
+        client_secret: process.env.CLIENT_SECRET_GITHUB,
+        code,
+    }
+    const {data: token} = await axios.post('https://github.com/login/oauth/access_token',body,opts)
+    res.redirect(`/posts/${token.access_token}`)
+})
+
+router.get('/allposts', async (req,res) => {
+    try {
+        await mongoose.connect(process.env.DB_URL, {useUnifiedTopology: true , useNewUrlParser: true })
+        const conn = await mongoose.connection
+        const posts = await conn.collection('posts').find({}).toArray()
+        return res.status(200).send(posts)
+    }catch(err) {
+        if(err) console.log(`Erro ao pegar posts::: ${err}`)
+    }
+})
 router.post('/create', async (req,res) => {
     try {
         const post = {
             title: `${req.body.title}`,
             description: `${req.body.description}`,
             likes: 0,
-            comments: []
+            comments: []    
         }
         const data = await Post.create(post)
         if(data._id) return res.status(200).send(data)
