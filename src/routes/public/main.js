9f536c1fa4d2677c6014dbb889250b9c7a181b8b
lemao-code <th.ferrari@outlook.com.br>
2021-08-30 22:30:22 -0300

    create elements in html

--- src/routes/public/main.js
+++ src/routes/public/main.js
@@ -1,6 +1,24 @@
-console.log('foi')
 
-function redirectUrl() {
-    console.log('1????')
-    window.location.href='/guest/posts'
+
+
+
+function createTimeLinePosts(posts) {
+        console.log(posts)
+        let post = document.createElement("p")
+        post.innerHTML = posts.title
+        return post
 }
+
+async function getPosts() {
+    fetch('/allposts')
+    .then((res) => res.json())
+    .then((posts) => {
+        const doc = document.getElementById("tester")
+        posts.forEach(element => {
+            let post = createTimeLinePosts(element)
+            doc.appendChild(post)
+
+    })        
+    })
+}
+getPosts()
